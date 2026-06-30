51dec9286c34ac14e2831cc8a44168cd8f1f642c
DrabornEagle <draborneagle@gmail.com>
2026-07-01 01:54:06 +0300

    Bump app status to v1.0.6

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,2 +1,2 @@
-export const APP_STATUS_CODE = 'v1.0.5';
-export const APP_VERSION_LABEL = 'DraBornLife v1.0.5';
+export const APP_STATUS_CODE = 'v1.0.6';
+export const APP_VERSION_LABEL = 'DraBornLife v1.0.6';
