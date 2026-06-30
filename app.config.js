dc3f76622936a4b2fc4c21cbb95d3f328c9947b9
DrabornEagle <draborneagle@gmail.com>
2026-06-30 22:21:43 +0300

    config 049

--- app.config.js
+++ app.config.js
@@ -2,20 +2,20 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '0.4.8',
+    version: '0.4.9',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
     assetBundlePatterns: ['**/*'],
     android: {
       package: 'com.draborneagle.drabornlife',
-      versionCode: 48,
+      versionCode: 49,
     },
     ios: {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v0.4.8',
+      statusCode: 'v0.4.9',
       storageMode: 'local-only',
       cloudDatabase: false,
       apkFirstVersion: 'v1.0',
