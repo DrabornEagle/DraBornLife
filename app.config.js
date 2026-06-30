bf3365277085ef3b9e9afbba868ffa09566239f2
DrabornEagle <draborneagle@gmail.com>
2026-06-30 17:49:02 +0300

    v0.2.7: update Expo config version

--- app.config.js
+++ app.config.js
@@ -2,20 +2,20 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '0.2.6',
+    version: '0.2.7',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
     assetBundlePatterns: ['**/*'],
     android: {
       package: 'com.draborneagle.drabornlife',
-      versionCode: 26,
+      versionCode: 27,
     },
     ios: {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v0.2.6',
+      statusCode: 'v0.2.7',
       storageMode: 'local-only',
       cloudDatabase: false,
       apkFirstVersion: 'v1.0',
