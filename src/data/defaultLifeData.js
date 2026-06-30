536c3bad938190c2334acf355ecc7b0d85c513e6
DrabornEagle <draborneagle@gmail.com>
2026-06-30 23:18:41 +0300

    storage 051

--- src/data/defaultLifeData.js
+++ src/data/defaultLifeData.js
@@ -1,9 +1,9 @@
-export const STORAGE_VERSION = 'v0.4.10';
+export const STORAGE_VERSION = 'v0.5.1';
 
 export const defaultLifeData = {
   settings: {
     appName: 'DraBornLife',
-    currentVersionCode: 'v0.4.10',
+    currentVersionCode: 'v0.5.1',
     selectedYear: 2026,
     targetCity: 'Antalya',
     targetAreas: ['Muratpasa', 'Lara', 'Konyaalti'],
