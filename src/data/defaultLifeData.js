0fdd7c00677edbe859dd53c48b2720359c2a8bc8
DrabornEagle <draborneagle@gmail.com>
2026-07-01 02:37:19 +0300

    Update default storage version for v1.0.9

--- src/data/defaultLifeData.js
+++ src/data/defaultLifeData.js
@@ -1,9 +1,9 @@
-export const STORAGE_VERSION = 'v1.0.8';
+export const STORAGE_VERSION = 'v1.0.9';
 
 export const defaultLifeData = {
   settings: {
     appName: 'DraBornLife',
-    currentVersionCode: 'v1.0.8',
+    currentVersionCode: 'v1.0.9',
     selectedYear: 2026,
     targetCity: 'Antalya',
     targetAreas: ['Muratpaşa', 'Lara', 'Konyaaltı'],
