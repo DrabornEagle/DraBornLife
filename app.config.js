56c7a205c9080930ad10f4c3fa93a7c78fe2c7fd
DrabornEagle <draborneagle@gmail.com>
2026-06-30 19:34:09 +0300

    v0.3.6 app config

--- app.config.js
+++ app.config.js
@@ -2,20 +2,20 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '0.3.5',
+    version: '0.3.6',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
     assetBundlePatterns: ['**/*'],
     android: {
       package: 'com.draborneagle.drabornlife',
-      versionCode: 35,
+      versionCode: 36,
     },
     ios: {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v0.3.5',
+      statusCode: 'v0.3.6',
       storageMode: 'local-only',
       cloudDatabase: false,
       apkFirstVersion: 'v1.0',
