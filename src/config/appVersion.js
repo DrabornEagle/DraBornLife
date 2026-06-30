c3b67076797bf909d1472e7964443d34abed1d09
DrabornEagle <draborneagle@gmail.com>
2026-07-01 02:53:51 +0300

    Set clean version label

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,2 +1,2 @@
-export const APP_STATUS_CODE = 'v1.0.11-hotfix';
-export const APP_VERSION_LABEL = 'DraBornLife v1.0.11-hotfix';
+export const APP_STATUS_CODE = 'v1.0.11';
+export const APP_VERSION_LABEL = 'DraBornLife v1.0.11';
