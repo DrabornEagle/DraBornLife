cf34dd39104b940f094a316edf4fb2cf21427f18
DrabornEagle <draborneagle@gmail.com>
2026-07-02 01:35:19 +0300

    Update label

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,2 +1,2 @@
-export const APP_STATUS_CODE = 'v1.4.9';
-export const APP_VERSION_LABEL = 'DraBornLife v1.4.9';
+export const APP_STATUS_CODE = 'v1.4.10';
+export const APP_VERSION_LABEL = 'DraBornLife v1.4.10';
