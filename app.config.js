1246e1f97ef51cccee76596780e0d6a42c6507ac
DrabornEagle <draborneagle@gmail.com>
2026-07-01 14:07:15 +0300

    Update Expo config for v1.2.3 without APK build

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '1.1.0',
+    version: '1.2.3',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
@@ -15,7 +15,7 @@ module.exports = {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v1.1',
+      statusCode: 'v1.2.3',
       stabilizationPhaseClosed: true,
       storageMode: 'local-only',
       cloudDatabase: false,
