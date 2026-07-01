aa75962d38e571ecd2c50d22de301620d372684c
DrabornEagle <draborneagle@gmail.com>
2026-07-02 00:54:57 +0300

    Set config version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '1.4.3',
+    version: '1.4.4',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
@@ -15,7 +15,7 @@ module.exports = {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v1.4.3',
+      statusCode: 'v1.4.4',
       stabilizationPhaseClosed: true,
       storageMode: 'local-only',
       cloudDatabase: false,
