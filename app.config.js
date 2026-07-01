4788c474f35b9d8dae9e6a63d9ad26afd2e9e923
DrabornEagle <draborneagle@gmail.com>
2026-07-02 01:21:22 +0300

    Update config

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'DraBornLife',
     slug: 'drabornlife',
-    version: '1.4.6',
+    version: '1.4.7',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     scheme: 'drabornlife',
@@ -15,7 +15,7 @@ module.exports = {
       bundleIdentifier: 'com.draborneagle.drabornlife',
     },
     extra: {
-      statusCode: 'v1.4.6',
+      statusCode: 'v1.4.7',
       stabilizationPhaseClosed: true,
       storageMode: 'local-only',
       cloudDatabase: false,
